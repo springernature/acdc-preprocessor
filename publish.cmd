f003baed753ed4e2b9b20b7061b9ae96c645f5fd
raut06 <raut06@LAINPU1502-0017>
2021-01-11 14:33:35 +0530

    test

--- publish.cmd
+++ publish.cmd
@@ -5,7 +5,7 @@ set FRAMEWORK_PARAM=--framework netcoreapp3.1
 set CONFIGURATION_PARAM=--configuration Release
 
 set CONSOLE_CSPROJ=.\src\Acdc.Preprocessor.Console\Acdc.Preprocessor.Console.csproj
-set CONSOLE_OUTPUT_PARAM=--output "..\..\publish\Acdc.Preprocessor.Console\\"
+set CONSOLE_OUTPUT_PARAM=--output ".\publish\Acdc.Preprocessor.Console\\"
 
 set CONSOLE_DOTNET_PARAMS=publish %CONSOLE_CSPROJ% %CONSOLE_OUTPUT_PARAM% %FRAMEWORK_PARAM% %CONFIGURATION_PARAM%
 
